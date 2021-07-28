f85fe3f39b551ddd0b080125e3ca7fcd85c6338b
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-07-28 10:01:34 -0700

    connect list to app

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import ArticArtworks from '../../containers/ArticArtworks';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <ArticArtworks />;
 }
