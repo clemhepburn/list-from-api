8a26d2d032fc9ccda370b4192f573849a684ff10
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-07-28 09:54:18 -0700

    create List component

---
+++ src/components/artworks/ArtworkList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Artwork from './Artwork';
+
+const ArtworkList = ({ artworks }) => {
+  const artworkElements = artworks.map((artwork) => (
+    <li key={artwork.id}>
+      <Artwork
+        {...artwork}
+      />
+    </li>
+  ));
+
+  return <ul>{artworkElements}</ul>;
+};
+
+ArtworkList.propTypes = {
+  artworks: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    artist: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+export default ArtworkList;
+
